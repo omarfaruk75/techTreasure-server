e9663a2dc62e57df3bb93eece02aad8e0b913245
omar faruk <farukomarcpa@gmail.com>
2024-06-09 00:27:56 +0600

    delete api for rejected product added

--- index.js
+++ index.js
@@ -397,6 +397,12 @@ const query = {_id:new ObjectId(id)}
 const result=await productsCollection.deleteOne(query)
 res.send(result);
 })
+app.delete('/product/reject/:id',async(req,res)=>{
+const id=req.params.id;
+const query = {_id:new ObjectId(id)}
+const result=await productsCollection.deleteOne(query)
+res.send(result);
+})
 
 
 
