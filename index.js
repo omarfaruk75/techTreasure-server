c0da5e207405dd750cc96a395bbd7fd54db15385
omar faruk <farukomarcpa@gmail.com>
2024-06-10 14:28:16 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -479,8 +479,8 @@ res.send(result);
 
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
   
   }
